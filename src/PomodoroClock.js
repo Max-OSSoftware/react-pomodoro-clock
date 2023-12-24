0bf07f15fca6b57d0778b43b56ae8b08c730bb3a
zapboy216 <zapboy216@netzero.com>
2023-12-24 12:50:14 -0500

    Refactor timer logic and UI

--- src/PomodoroClock.js
+++ src/PomodoroClock.js
@@ -5,27 +5,23 @@ class PomodoroClock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            breakLength: 5,
-            sessionLength: 25,
+            breakLength: 5,  // Break length 5 minutes
+            sessionLength: 25, // Session length 25 minutes
             timerLabel: 'Session',
-            timeLeft: 25 * 60, // in seconds
+            timeLeft: 25 * 60, // Time left in seconds
             isRunning: false,
-            intervalID: null,
+            intervalID: null
         };
     }
 
-    handleIncrement = (type) => {
-        if (this.state[type] < 60) {
-            this.setState({ [type]: this.state[type] + 1 });
-        }
+    // Format time to mm:ss
+    formatTime = (timeInSeconds) => {
+        let minutes = Math.floor(timeInSeconds / 60);
+        let seconds = timeInSeconds % 60;
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
 
-    handleDecrement = (type) => {
-        if (this.state[type] > 1) {
-            this.setState({ [type]: this.state[type] - 1 });
-        }
-    }
-
+    // Toggle start and stop of the timer
     toggleTimer = () => {
         if (this.state.isRunning) {
             clearInterval(this.state.intervalID);
@@ -36,100 +32,106 @@ class PomodoroClock extends React.Component {
         }
     }
 
+    // Decrement the timer
     decrementTimer = () => {
         if (this.state.timeLeft > 0) {
-            this.setState({ timeLeft: this.formatTime(this.state.timeLeft - 1) });
+            this.setState({ timeLeft: this.state.timeLeft - 1 });
         }
-        if (this.state.timeLeft === '00:00') {
+        if (this.state.timeLeft === 0) {
             this.playBeep();
             this.switchTimer();
         }
-    };
-    
-
-    switchTimer = () => {
-        if (this.state.timerLabel === 'Session') {
-            this.setState({
-                timerLabel: 'Break',
-                timeLeft: this.state.breakLength * 60
-            });
-        } else {
-            this.setState({
-                timerLabel: 'Session',
-                timeLeft: this.state.sessionLength * 60
-            });
-        }
     }
 
+    // Switch between session and break
+    switchTimer = () => {
+        let newLabel = this.state.timerLabel === 'Session' ? 'Break' : 'Session';
+        let newTime = (newLabel === 'Session' ? this.state.sessionLength : this.state.breakLength) * 60;
+        this.setState({
+            timerLabel: newLabel,
+            timeLeft: newTime
+        });
+    }
+
+    // Handle reset
     handleReset = () => {
         clearInterval(this.state.intervalID);
         this.setState({
             breakLength: 5,
             sessionLength: 25,
             timerLabel: 'Session',
-            timeLeft: this.formatTime(25 * 60),
+            timeLeft: 25 * 60,
             isRunning: false,
             intervalID: null,
         });
         this.stopBeep();
     }
 
-    formatTime = (timeInSeconds) => {
-        let minutes = Math.floor(timeInSeconds / 60);
-        let seconds = timeInSeconds % 60;
-        return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-    }
-
-
+    // Play beep sound
     playBeep = () => {
         document.getElementById('beep').play();
     }
 
+    // Stop and reset beep sound
     stopBeep = () => {
         const audio = document.getElementById('beep');
         audio.pause();
         audio.currentTime = 0;
     }
 
+    // Change timer (increment/decrement session/break)
+    changeTimer = (operator, type) => {
+        const value = this.state[type];
+        if (operator === 'decrement' && value > 1) {
+            this.setState({
+                [type]: value - 1,
+                timeLeft: (type === this.state.timerLabel.toLowerCase() + 'Length') 
+                          ? (value - 1) * 60 
+                          : this.state.timeLeft
+            });
+        }
+        if (operator === 'increment' && value < 60) {
+            this.setState({
+                [type]: value + 1,
+                timeLeft: (type === this.state.timerLabel.toLowerCase() + 'Length') 
+                          ? (value + 1) * 60 
+                          : this.state.timeLeft
+            });
+        }
+    };
+
     render() {
         return (
             <div className="container text-center">
-                <h1 className="text-decoration-underline" >Pomodoro Clock</h1>
-                <p className="fw-normal fs-6">by zapboy216</p>
-
+                <h1>A Pomodoro Clock</h1>
                 <div id="timer-label">{this.state.timerLabel}</div>
                 <div id="time-left">{this.formatTime(this.state.timeLeft)}</div>
-                
-
                 <button id="start_stop" className="btn btn-primary" onClick={this.toggleTimer}>
                     Start/Stop
                 </button>
                 <button id="reset" className="btn btn-danger" onClick={this.handleReset}>
                     Reset
                 </button>
-
                 <div className="length-control">
                     <div id="break-label">Break Length</div>
-                    <button id="break-decrement" className="btn btn-secondary" onClick={() => this.handleDecrement('breakLength')}>
+                    <button id="break-decrement" className="btn btn-secondary" onClick={() => this.changeTimer('decrement', 'breakLength')}>
                         -
                     </button>
                     <span id="break-length">{this.state.breakLength}</span>
-                    <button id="break-increment" className="btn btn-secondary" onClick={() => this.handleIncrement('breakLength')}>
+                    <button id="break-increment" className="btn btn-secondary" onClick={() => this.changeTimer('increment', 'breakLength')}>
                         +
                     </button>
                 </div>
-
                 <div className="length-control">
                     <div id="session-label">Session Length</div>
-                    <button id="session-decrement" className="btn btn-secondary" onClick={() => this.handleDecrement('sessionLength')}>
+                    <button id="session-decrement" className="btn btn-secondary" onClick={() => this.changeTimer('decrement', 'sessionLength')}>
                         -
                     </button>
                     <span id="session-length">{this.state.sessionLength}</span>
-                    <button id="session-increment" className="btn btn-secondary" onClick={() => this.handleIncrement('sessionLength')}>
+                    <button id="session-increment" className="btn btn-secondary" onClick={() => this.changeTimer('increment', 'sessionLength')}>
                         +
                     </button>
                 </div>
-
                 <audio id="beep" src="https://actions.google.com/sounds/v1/alarms/beep_short.ogg"></audio>
             </div>
         );
